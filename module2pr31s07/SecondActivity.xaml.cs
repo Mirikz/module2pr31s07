b6ace8620fe7cf699f83bdc504a243c2e6bafb39
310-07 <310-07@310-07>
2021-12-08 11:50:34 +0500

    Добавьте файлы проекта.

---
+++ module2pr31s07/SecondActivity.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace module2pr31s07
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondActivity : ContentPage
+    {
+        public SecondActivity()
+        {
+            InitializeComponent();
+        }
+    }
+}
